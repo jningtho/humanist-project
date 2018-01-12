7bc8967b021e9d94eb5adf234ccf6041b03eed8f
jningtho <jningtho@x-designs.net>
2018-01-12 16:52:05 +0530

    changes 2

--- app.js
+++ app.js
@@ -3,5 +3,13 @@ const app = express();
 
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(__dirname + 'index.html');
 })
+
+// app.listen(3000);
+
+// var port = process.env.PORT || 3000,
+//     http = require('http'),
+//     fs = require('fs'),
+//     html = fs.readFileSync('public/index.html');
+
